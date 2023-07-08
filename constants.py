8d1c20031092eb953c99a386fa2744aef20de700
Apexmavric <97466518+Apexmavric@users.noreply.github.com>
2023-07-09 02:22:12 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,45 @@
+BASE_URL = "https://www.makemytrip.com/"
+SEC_URL = "https://www.makemytrip.com/railways/"
+class P:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+pstates = {(121,131):"Haryana",
+               (744,744):"Andaman and Nicobar",
+               (110,110):"Delhi",
+               (396,396):"Dadra and Nagar Haveli",
+               (490,497):"Chhattisgarh",
+               (781,788):"Assam",
+               (790,792):"Arunachal Pradesh",
+               (797,798):"Nagaland",
+               (682,682): "Lakshadweep",
+               (500,509): "Telangana",
+               (737,737): "Sikkim",
+               (700,743): "West Bengal",
+               (813,835): "Jharkhand",
+               (783,794): "Meghalaya",
+               (751,770): "Odisha",
+               (244,263): "Uttarakhand",
+               (180,194): "Jammu and Kashmir",
+               (799,799): "Tripura",
+               (796,796): "Mizoram",
+               (301,345): "Rajasthan",
+               (795,795): "Manipur",
+               (360,396): "Gujarat",
+               (403,403): "Goa",
+               (800,855): "Bihar",
+               (507,535): "Andhra Pradesh",
+               (560,591): "Karnataka",
+               (362,396): "Daman and Diu",
+               (400,445): "Maharashtra",
+               (450,488): "Madhya Pradesh",
+               (201,285): "Uttar Pradesh",
+               (670,695): "Kerala",
+               (140,160): "Chandigarh",
+               (600,643): "Tamil Nadu",
+               (533,673): "Puducherry",
+               (140,160): "Punjab",
+               (171,177): "Himachal Pradesh"
+               }
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
---
+++ mainfile.py
@@ -0,0 +1,20 @@
+import constants
+P=constants.P
+pe = []
+
+type = "t"
+Start_City = "Mumbai"
+End_City = "Ahemdabad"
+Start_Date = "17-07-2023"
+Return_Date ="1-08-2023"
+pclass = "3A"
+pe.append(P("Vidhit Patel",21,"m"))
+pe.append(P("Harsha Patel",51,"f"))
+pe.append(P("Atul Patel",53,"m"))
+pe.append(P("Pranali Patel",25,"f"))
+pe.append(P("Random Kid",10,"f"))
+User_Name ="apexmavric234@gmail.com"
+Pass = 'Apexmavric@243'
+irctc = 'Apexmavric'
+Pincode = '400077'
+Mobile = "8879699467"
